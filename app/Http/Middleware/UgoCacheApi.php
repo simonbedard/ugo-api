328962854ceb97169d0e420479a1e57d5225c3e0
Simon Bédard <simon@lesmauvaisgarcons.ca>
2023-02-17 14:12:23 -0500

    CleanUp Search by id function

--- app/Http/Middleware/UgoCacheApi.php
+++ app/Http/Middleware/UgoCacheApi.php
@@ -17,6 +17,9 @@ class UgoCacheApi
      */
     public function handle(Request $request, Closure $next)
     {
+
+        return $next($request);
+        
         if(Cache::has($request->fullUrl())) {
             $response = Cache::get($request->fullUrl())->response()->header('X-Ugo-Cache', 'hit');
             return $response;
--- app/Search/Search.php
+++ app/Search/Search.php
@@ -10,17 +10,18 @@ use Illuminate\Support\Facades\Log;
 
 class Search
 {
+
+    protected mixed $time_start;
+    protected mixed $time_end;
+
     function __construct() {
+
+
     }
     /**
      * Search Api's by single terms
      */
     public function byTerms(Request $request, string $term, string $page){
-        /*
-        $time_start = microtime(true);
-        $time_end = microtime(true);
-        $execution_time = ($time_end - $time_start);*/
-
         // Create the search query options
         $queryOptions = [
             "name" => "byTerms",
@@ -38,11 +39,62 @@ class Search
         // Cache the response
         $this->setCache($request, $searchQuery);
         
-
-        // $this->SetAnalytics($execution_time);
         return $response;
 
     }
+
+    /**
+     * Search file by unique id
+     * @param $provider
+     * @param $id 
+     */
+    public function byId(Request $request, $provider, $id){
+
+        /**
+         * Initialise Analitycs
+         */
+        $this->time_start = microtime(true);
+        $this->time_end = microtime(true);
+        /**
+         * Single Search query options
+         */
+        $queryOptions = [
+            "name" => "byId",
+            "id" => $id,
+            "provider" => $provider,
+        ];
+
+        /**
+         * Create the query and run it
+         */
+        $searchQuery = (new SearchQuerySingle($queryOptions))->run();
+        
+        /**
+         * Format the response
+         */
+        $response = $searchQuery->response()->header('X-Ugo-Cache', 'miss');
+        
+        /**
+         * Set query in cache
+         */
+        $this->setCache($request, $searchQuery);
+    
+        /**
+         * Log query analitycs
+         */
+        $execution_time = ($this->time_end - $this->time_start);
+        dump($execution_time);
+        $this->SetAnalytics($execution_time);
+
+        return $response;
+    }
+
+    private function setCache(Request $request, SearchQuery|SearchQuerySingle $searchQuery){
+
+        // REFACTOR IF CACHE IS ENABLE
+        Cache::put($request->fullUrl(), $searchQuery); 
+    }
+
 
     public function test(Request $request, string $term, string $page){
         /*
@@ -75,30 +127,7 @@ class Search
 
     }
 
-    public function byId(Request $request, $provider, $id){
-
-        $queryOptions = [
-            "name" => "byId",
-            "id" => $id,
-            "provider" => $provider,
-        ];
-        // Create and run the query
-        $searchQuery = (new SearchQuerySingle($queryOptions))->run();
-        $response = $searchQuery->response()->header('X-Ugo-Cache', 'miss');
-        
-        $this->setCache($request, $searchQuery);
     
-        return $response;
-    }
-
-    private function setCache(Request $request, SearchQuery|SearchQuerySingle $searchQuery){
-
-        // REFACTOR IF CACHE IS ENABLE
-        Cache::put($request->fullUrl(), $searchQuery); 
-    }
-
-    
-
     /**
      * Set analitycs data after request is done
      */
